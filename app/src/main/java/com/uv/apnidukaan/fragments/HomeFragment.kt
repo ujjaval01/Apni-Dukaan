4cd4413769fab8d86b2e0a313b74cc768775454c
Ujjaval Saini <sainiujvl@gmail.com>
2025-09-02 16:23:52 +0530

    update time and date, make realtime runnable in home screen

--- app/src/main/java/com/uv/apnidukaan/fragments/HomeFragment.kt
+++ app/src/main/java/com/uv/apnidukaan/fragments/HomeFragment.kt
@@ -2,9 +2,12 @@ package com.uv.apnidukaan.fragments
 
 import android.graphics.Color
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -15,10 +18,18 @@ import com.github.mikephil.charting.data.LineDataSet
 import com.uv.apnidukaan.R
 import com.uv.apnidukaan.adapter.TransactionAdapter
 import com.uv.apnidukaan.model.Transaction
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
 
 class HomeFragment : Fragment() {
 
     private lateinit var chart: LineChart
+
+    //for time and date
+    private lateinit var tvDateTime: TextView
+    private val handler = Handler(Looper.getMainLooper())
+    private lateinit var runnable: Runnable
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -26,6 +37,10 @@ class HomeFragment : Fragment() {
     ): View? {
         val view = inflater.inflate(R.layout.fragment_home, container, false)
 
+        //for date and time
+        tvDateTime = view.findViewById(R.id.tvDateTime)
+
+        //for chart with dummy data
         chart = view.findViewById(R.id.profitLossChart)
         setupProfitLossChart()
 
@@ -79,5 +94,23 @@ class HomeFragment : Fragment() {
         chart.description.isEnabled = false
         chart.animateY(1000)
         chart.invalidate()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        runnable = object : Runnable {
+            override fun run() {
+                val currentDateTime = SimpleDateFormat("dd/MM/yyyy HH:mm:ss", Locale.getDefault())
+                    .format(Date())
+                tvDateTime.text = currentDateTime
+                handler.postDelayed(this, 1000)
+            }
+        }
+        handler.post(runnable)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        handler.removeCallbacks(runnable)
     }
 }
